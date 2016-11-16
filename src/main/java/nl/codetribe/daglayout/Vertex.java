44f76d448fb78d3098fed87004ac0063841689c3
Ron Smits <ron.smits@nasdaq.com>
2016-11-16 14:27:49 +0100

    kinda does something

--- src/main/java/nl/codetribe/daglayout/Vertex.java
+++ src/main/java/nl/codetribe/daglayout/Vertex.java
@@ -4,6 +4,7 @@ import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.SimpleDoubleProperty;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
@@ -14,48 +15,74 @@ import javafx.scene.text.TextBoundsType;
 /**
  * Created by ronsmits on 15/11/2016.
  */
-public class Vertex extends StackPane{
+public class Vertex extends Pane
+{
 
-    private DoubleProperty bottomPortXProperty = new SimpleDoubleProperty();
-    private DoubleProperty bottomPortYProperty = new SimpleDoubleProperty();
+    public DoubleProperty getBottomPortXProperty()
+    {
+        return bottomPortXProperty;
+    }
+
+    public DoubleProperty getBottomPortYProperty()
+    {
+        return bottomPortYProperty;
+    }
+
+
+
+    private SimpleDoubleProperty bottomPortXProperty = new SimpleDoubleProperty();
+    private SimpleDoubleProperty bottomPortYProperty = new SimpleDoubleProperty();
 
     public void setBottomPortX(double value) { bottomPortXProperty.setValue(value);}
     public void setBottomPortY(double value) {bottomPortYProperty.setValue(value);}
 
     public Vertex(String label){
+        StackPane stackPane = new StackPane();
         Rectangle rectangle = new Rectangle(100, 50, Color.THISTLE);
         rectangle.setStroke(Color.BLACK);
         Text text = new Text(label);
+
         text.setBoundsType(TextBoundsType.LOGICAL_VERTICAL_CENTER);
         //text.setWrappingWidth(95);
-        getChildren().addAll(rectangle, text);
+        stackPane.getChildren().addAll(rectangle, text);
+        getChildren().add(stackPane);
+
+        bottomPortXProperty.setValue(50);
+        bottomPortYProperty.setValue(50);
         layoutXProperty().addListener(new ChangeListener<Number>() {
             @Override
             public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
-                System.out.println("new value is "+newValue.doubleValue());
+                System.out.println(label);
                 bottomPortXProperty.setValue(newValue.doubleValue()+50.0);
+                printBottomPort();
             }
         });
         layoutYProperty().addListener(new ChangeListener<Number>() {
             @Override
             public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
-                System.out.println("Y "+newValue);
+                System.out.println(label);
                 bottomPortYProperty.setValue(newValue.doubleValue()+getHeight());
-                System.out.println(bottomPortYProperty);
+                printBottomPort();
             }
         });
 
-        Circle circle = new Circle(5, Color.RED);
-        circle.centerXProperty().bind(bottomPortXProperty);
-        circle.centerYProperty().bind(bottomPortYProperty);
+        Circle circle = new Circle(6, Color.RED);
+        System.out.println(getLayoutBounds());
+        circle.setCenterX(50);
+        circle.setCenterY(50);
+
         circle.centerYProperty().addListener(new ChangeListener<Number>() {
             @Override
             public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
                 System.out.println("circle Y is now "+newValue);
+                System.out.println(getLayoutBounds());
             }
         });
         getChildren().add(circle);
     }
 
+    private void printBottomPort() {
+        System.out.println(String.format("X: %f Y: %f", bottomPortXProperty.doubleValue(), bottomPortYProperty.doubleValue()));
+    }
 
 }
--- src/test/java/TestMain.java
+++ src/test/java/TestMain.java
@@ -2,13 +2,11 @@ import javafx.application.Application;
 import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Line;
 import javafx.stage.Stage;
 import nl.codetribe.daglayout.Vertex;
-
-import static javafx.scene.input.KeyCode.UP;
 
 /**
  * Created by ronsmits on 15/11/2016.
@@ -19,21 +17,36 @@ public class TestMain extends Application {
         final Scene scene = new Scene(group, 600, 400, Color.CORNSILK);
 
         Vertex e = new Vertex("test");
+        Vertex b = new Vertex("nog een test");
         scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
             @Override
             public void handle(KeyEvent event) {
                 switch (event.getCode()) {
-                    case UP: e.relocate(e.getLayoutX(), e.getLayoutY()+5);break;
+                    case UP: e.relocate(e.getLayoutX(),      e.getLayoutY()-5);break;
+                    case DOWN: e.relocate(e.getLayoutX(),    e.getLayoutY()+5);break;
+                    case LEFT: e.relocate(e.getLayoutX()-5,  e.getLayoutY());break;
+                    case RIGHT: e.relocate(e.getLayoutX()+5, e.getLayoutY());break;
                 }
             }
         });
 
-        group.getChildren().add(e);
-        e.relocate(100, 50);
+        group.getChildren().addAll(e, b);
+
+//        e.relocate(100, 50);
         e.relocate(100,100);
+        Line line = new Line();
+        line.startXProperty().bind(e.getBottomPortXProperty());
+        line.startYProperty().bind(e.getBottomPortYProperty());
+        line.endXProperty().bind(b.getBottomPortXProperty());
+        line.endYProperty().bind(b.getBottomPortYProperty());
+        group.getChildren().add(line);
+        b.relocate(10,10);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
 
+    public static void main(String[] args) {
+        launch(args);
+    }
 
 }
